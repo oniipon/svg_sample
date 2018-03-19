8df9f4189a6286ad1ccc4aed002b95ea10ca6fea
oniipon <oniipon@gmail.com>
2018-03-19 15:28:07 +0900

    ドラッグアンドドロップ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as d3 from 'd3';
+import {OnInit} from '@angular/core/src/metadata/lifecycle_hooks';
+import {ScaleLinear, ScaleBand} from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  width = 400;
+  height = 600;
+  xMin = 2;
+  xMax = 12;
+  yMin = 0;
+  yMax = 1;
+  xScale: ScaleBand<string>;
+  yScale: ScaleLinear<number, number>;
+  xTick: XTick[];
+  yTick: [number, number];
+  circleX = 300;
+  circleY = 300;
+  dragFlag = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.createXtick();
+    this.yScale = this.getYscale();
+  }
+
+  private getXscale(x: string) {
+    return d3
+      .scaleBand()
+      .rangeRound([0, this.width])
+      .padding(2)
+      .domain(this.xTick.map((d) => {
+        return d.value;
+      }))(x);
+  }
+
+  private getYscale() {
+    return d3
+      .scaleLinear()
+      .domain([this.yMin, this.yMax])
+      .range([0, this.height]);
+  }
+
+  private createXtick() {
+    const result = [];
+    for (let i = 2; i <= 12; i++) {
+      result.push({value: i.toString()});
+    }
+    this.xTick = result;
+  }
+
+  move(event: MouseEvent) {
+    if (!this.dragFlag) return;
+    this.circleX = event.clientX;
+    this.circleY = event.clientY;
+  }
+}
+
+export class XTick {
+  value: string;
 }
---
+++ src/app/sum.ts
@@ -0,0 +1,5 @@
+export class Sum {
+  public sum(x: number, y: number): number {
+    return x + y;
+  }
+}
